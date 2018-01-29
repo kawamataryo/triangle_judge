632b08b671e1fc26452344d7f7e576f0d02a79b5
ryo <ba068082@gmail.com>
2018-01-29 16:57:55 +0900

    コマンドライン引数の出力処理実装

--- spec/triangle_judge_spec.rb
+++ spec/triangle_judge_spec.rb
@@ -1,25 +1,25 @@
-require "./triangle_judge"
+require "./lib/triangle_judge"
 
 RSpec.describe "triangle_judge" do
   it "正三角形" do
-    expect(triangle_judge(3,3,3)).to eq("正三角形ですね！")
+    expect(triangle_judge([3, 3, 3])).to eq("正三角形ですね！")
   end
   it "二等辺三角形" do
-    expect(triangle_judge(2,2,1)).to eq("二等辺三角形ですね！")
+    expect(triangle_judge([2, 2, 1])).to eq("二等辺三角形ですね！")
   end
   it "不等辺三角形" do
-    expect(triangle_judge(2,3,4)).to eq("不等辺三角形ですね！")
+    expect(triangle_judge([2, 3, 4])).to eq("不等辺三角形ですね！")
   end
   it "最大の長さの辺が他の辺の合計を超える" do
-    expect(triangle_judge(1,2,3)).to eq("三角形じゃないです＞＜")
+    expect(triangle_judge([1, 2, 3])).to eq("三角形じゃないです＞＜")
   end
   it "辺の長さに0が含まれる" do
-    expect(triangle_judge(5,4,0)).to eq("三角形じゃないです＞＜")
+    expect(triangle_judge([5, 4, 0])).to eq("三角形じゃないです＞＜")
   end
   it "辺が多い" do
-    expect(triangle_judge(5,4,3,2)).to eq("三角形じゃないです＞＜")
+    expect(triangle_judge([5, 4, 3, 2 ])).to eq("三角形じゃないです＞＜")
   end
   it "辺が少ない" do
-    expect(triangle_judge(5,4)).to eq("三角形じゃないです＞＜")
+    expect(triangle_judge([5, 4])).to eq("三角形じゃないです＞＜")
   end
 end
---
+++ triangle.rb
@@ -0,0 +1,3 @@
+require "./lib/triangle_judge"
+
+puts triangle_judge(ARGV.map(&:to_i))
