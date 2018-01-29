07090b39b5fcca2c3eef57af60f391b148db8bb1
ryo <ba068082@gmail.com>
2018-01-29 16:27:29 +0900

    二等辺三角形、不等辺三角形のチェック作成

--- spec/triangle_judge_spec.rb
+++ spec/triangle_judge_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe "triangle_judge" do
   it "正三角形" do
     expect(triangle_judge(1,1,1)).to eq("正三角形ですね！")
   end
+  it "二等辺三角形" do
+    expect(triangle_judge(2,2,1)).to eq("二等辺三角形ですね！")
+  end
+  it "不等辺三角形" do
+    expect(triangle_judge(2,3,4)).to eq("不等辺三角形ですね！")
+  end
 end
--- triangle_judge.rb
+++ triangle_judge.rb
@@ -1,5 +1,9 @@
 def triangle_judge(*sides)
   if sides.uniq.size == 1
     "正三角形ですね！"
+  elsif sides.uniq.size == 2
+    "二等辺三角形ですね！"
+  else
+    "不等辺三角形ですね！"
   end
 end
